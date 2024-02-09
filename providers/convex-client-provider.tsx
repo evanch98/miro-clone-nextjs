264b25ec4fee3e4f0112c66073f61f66342e3870
Kyaw Thu <evanch98@gmail.com>
2024-02-09 22:35:17 +0800

    update the convex-client-provider.tsx

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
