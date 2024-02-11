d0397b004b12bc21e27dcd9ae8a452a459a3e406
Kyaw Thu <evanch98@gmail.com>
2024-02-11 19:41:01 +0800

    update the DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "./_components/navbar";
 import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/sidebar";
 
@@ -17,7 +18,10 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
           <OrgSidebar />
-          <div className="h-full flex-1">{children}</div>
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </div>
     </main>
