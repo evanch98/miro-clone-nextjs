0db1485da3250f4f5689dbb4f5615ac19aeb6340
Kyaw Thu <evanch98@gmail.com>
2024-02-11 21:49:32 +0800

    update the List component

--- app/(dashboard)/_components/sidebar/list.tsx
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useOrganizationList } from "@clerk/nextjs";
+import { Item } from "./item";
 
 export const List = () => {
   const { userMemberships } = useOrganizationList({
@@ -14,7 +15,12 @@ export const List = () => {
   return (
     <ul className="space-y-4">
       {userMemberships.data?.map((mem) => (
-        <p key={mem.organization.id}>{mem.organization.name}</p>
+        <Item
+          key={mem.organization.id}
+          id={mem.organization.id}
+          name={mem.organization.name}
+          imageUrl={mem.organization.imageUrl}
+        />
       ))}
     </ul>
   );
