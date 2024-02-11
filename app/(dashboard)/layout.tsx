b5d30a9aa4912f86719ee2fe35ba558c668a74b8
Kyaw Thu <evanch98@gmail.com>
2024-02-11 19:30:34 +0800

    update the DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -13,7 +13,12 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">{children}</div>
+        </div>
+      </div>
     </main>
   );
 };
