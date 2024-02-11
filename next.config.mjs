5c2e406f5f405994b0ea54bf4d9b6c713c58ce10
Kyaw Thu <evanch98@gmail.com>
2024-02-11 21:50:36 +0800

    update the next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
+  },
+};
 
 export default nextConfig;
