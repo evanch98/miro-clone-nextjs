f9b0fa31f5210fbce8281a4ec0320267a152f5b2
Kyaw Thu <evanch98@gmail.com>
2024-02-11 19:23:32 +0800

    add docs

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,11 @@ interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
+/**
+ * @description Layout structure for Dashboard
+ * @param param0 children accepts React.ReactNode elements
+ * @returns JSX Elements
+ */
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return <div>{children}</div>;
 };
