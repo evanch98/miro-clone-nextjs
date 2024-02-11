66903cb7b84ca584a90ff93daa2bcf4f78af0e2b
Kyaw Thu <evanch98@gmail.com>
2024-02-11 21:46:27 +0800

    add a new component Item

---
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+
+interface ItemProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+export const Item = ({ id, name, imageUrl }: ItemProps) => {
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
+
+  const isActive = organization?.id === id;
+
+  const onClick = () => {
+    if (!setActive) return;
+    setActive({ organization: id });
+  };
+
+  return (
+    <div className="aspect-square relative">
+      <Image
+        src={imageUrl}
+        onClick={onClick}
+        alt={name}
+        fill
+        className={cn(
+          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+          isActive && "opacity-100"
+        )}
+      />
+    </div>
+  );
+};
