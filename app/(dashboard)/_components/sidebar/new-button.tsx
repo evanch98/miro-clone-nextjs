8cdc0f5cde4aeba0f827957d31ed7e012ba7f8bf
Kyaw Thu <evanch98@gmail.com>
2024-02-11 21:24:58 +0800

    update the NewButton

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -9,11 +9,14 @@ export const NewButton = () => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button>
+          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
             <Plus className="text-white" />
           </button>
         </div>
       </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+        <CreateOrganization />
+      </DialogContent>
     </Dialog>
   );
 };
