cd88337db348c6922e0032a383d7eb0c8f6da646
Kyaw Thu <evanch98@gmail.com>
2024-02-12 13:40:38 +0800

    update the Navbar component

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
 import { InviteButton } from "./invite-button";
 
 export const Navbar = () => {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
@@ -34,7 +40,7 @@ export const Navbar = () => {
           }}
         />
       </div>
-      <InviteButton />
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );
