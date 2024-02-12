a9dd9b40135e807b6847cfbc506005fa205898ed
Kyaw Thu <evanch98@gmail.com>
2024-02-12 14:27:52 +0800

    add a new component BoardList

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  return <div>{JSON.stringify(query)}</div>;
+};
