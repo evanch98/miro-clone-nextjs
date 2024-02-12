580924a5d40527fb40837fb3b20f247a92142351
Kyaw Thu <evanch98@gmail.com>
2024-02-12 13:20:46 +0800

    import the necessary packages

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+
 export const SearchInput = () => {
   return <div>Search Input</div>;
 };
