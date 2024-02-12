133f9accf96123f5d85a69c0647d5fc363d4b36f
Kyaw Thu <evanch98@gmail.com>
2024-02-12 13:17:14 +0800

    update the Navbar component

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5">
-      <div className="hidden lg:flex lg:flex-1">{/* TODO: Add Search */}</div>
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
       <UserButton />
     </div>
   );
