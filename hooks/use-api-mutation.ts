2ba02f3f6a959727bb28a50f8dfe500a8fe287ae
Kyaw Thu <evanch98@gmail.com>
2024-02-12 18:42:18 +0800

    add useApiMutation

---
+++ hooks/use-api-mutation.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "convex/react";
+import { useState } from "react";
+
+export const useApiMutation = (mutationFunction: any) => {
+  const [pending, setPending] = useState(false);
+  const apiMutation = useMutation(mutationFunction);
+
+  const mutate = (payload: any) => {
+    setPending(true);
+    return apiMutation(payload)
+      .finally(() => setPending(false))
+      .then((result) => {
+        return result;
+      })
+      .catch((e) => {
+        throw e;
+      });
+  };
+
+  return { mutate, pending };
+};
