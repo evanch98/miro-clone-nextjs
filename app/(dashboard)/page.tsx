e59f8eb522c1a0980a223ec83fff41d546f6e2a4
Kyaw Thu <evanch98@gmail.com>
2024-02-12 14:22:59 +0800

    update the DashboardPage

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 
 const DashboardPage = () => {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      <EmptyOrg />
+      {!organization ? <EmptyOrg /> : <p>Board List</p>}
     </div>
   );
 };
