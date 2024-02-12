e9419c38e47c8b3210b3e471f915038ad2f27992
Kyaw Thu <evanch98@gmail.com>
2024-02-12 13:30:45 +0800

    update the SearchInput component

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,11 +1,44 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { useRouter } from "next/navigation";
 import qs from "query-string";
 import { ChangeEvent, useEffect, useState } from "react";
-import { useDebounceCallback } from "usehooks-ts";
+import { useDebounceValue } from "usehooks-ts";
 
 export const SearchInput = () => {
-  return <div>Search Input</div>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounceValue(value, 500);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          search: debouncedValue[0],
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <div className="w-full relative">
+      <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+      <Input
+        className="w-full max-w-[516px] pl-9"
+        placeholder="Search boards"
+        onChange={handleChange}
+        value={value}
+      />
+    </div>
+  );
 };
