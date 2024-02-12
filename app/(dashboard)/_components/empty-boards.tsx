0b7aa70af37b795798377030e620983f2f175492
Kyaw Thu <evanch98@gmail.com>
2024-02-12 15:46:19 +0800

    update the EmptyBoards

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const EmptyBoards = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Image src="/note.svg" height={110} width={110} alt="Empty" />
+      <Image src="/note.svg" height={140} width={140} alt="Empty" />
       <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
       <p className="text-muted-foreground text-sm mt-2">
         Start by creating a board for your organization
