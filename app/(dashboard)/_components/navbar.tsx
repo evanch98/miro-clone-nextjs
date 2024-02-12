94eda9b8863c163d259280312fd5c04b00a398ac
Kyaw Thu <evanch98@gmail.com>
2024-02-12 13:37:04 +0800

    add the InviteButton

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
+import { InviteButton } from "./invite-button";
 
 export const Navbar = () => {
   return (
@@ -33,6 +34,7 @@ export const Navbar = () => {
           }}
         />
       </div>
+      <InviteButton />
       <UserButton />
     </div>
   );
