2cd3bf76b9c00e46f6436f4ca44ac400bbd69e80
Kyaw Thu <evanch98@gmail.com>
2024-02-12 14:31:00 +0800

    update the BoardList component

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -9,5 +9,19 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = []; // TODO: Change to API call
+
+  if (!data?.length && query.search) {
+    return <div>Try searching for something else</div>;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <div>No favorites</div>;
+  }
+
+  if (!data?.length) {
+    return <div>No boards at all</div>;
+  }
+
   return <div>{JSON.stringify(query)}</div>;
 };
