03ec970540c1d235bc6e97e9f2568dc0350e9664
Kyaw Thu <evanch98@gmail.com>
2024-02-13 12:54:10 +0800

    add a new component Footer

---
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { Star } from "lucide-react";
+
+interface FooterProps {
+  title: string;
+  authorLabel: string;
+  createdAtLabel: string;
+  isFavorite: boolean;
+  onClick: () => void;
+  disabled: boolean;
+}
+
+export const Footer = ({
+  title,
+  authorLabel,
+  createdAtLabel,
+  isFavorite,
+  onClick,
+  disabled,
+}: FooterProps) => {
+  return <div>Footer</div>;
+};
