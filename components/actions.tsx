b124a7c5afe7cde798e9efddd2bc2fd8975a0e18
Kyaw Thu <evanch98@gmail.com>
2024-02-13 14:09:25 +0800

    update the Actions component

--- components/actions.tsx
+++ components/actions.tsx
@@ -8,6 +8,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { Link2 } from "lucide-react";
+import { toast } from "sonner";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -24,9 +26,27 @@ export const Actions = ({
   id,
   title,
 }: ActionsProps) => {
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Failed to copy link"));
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
