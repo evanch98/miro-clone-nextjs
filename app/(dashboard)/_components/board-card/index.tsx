0352466f49b8c2e59c2f7d1247248c0505834459
Kyaw Thu <evanch98@gmail.com>
2024-02-13 14:03:09 +0800

    update the BoardCard component

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -8,6 +8,7 @@ import { useAuth } from "@clerk/nextjs";
 import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -42,7 +43,9 @@ export const BoardCard = ({
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
           <Actions id={id} title={title} side="right">
-            <button>Button</button>
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
           </Actions>
         </div>
         <Footer
