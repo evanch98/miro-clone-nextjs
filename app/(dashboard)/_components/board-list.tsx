dbccad82703418b99de886ba2e8e2336fee3265b
Kyaw Thu <evanch98@gmail.com>
2024-02-13 13:02:53 +0800

    update the BoardList component

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -42,6 +42,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         {query.favorites ? "Favorite boards" : "Team boards"}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
         {data.map((board) => (
           <BoardCard
             key={board._id}
