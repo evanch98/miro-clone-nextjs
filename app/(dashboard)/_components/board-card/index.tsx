095303dd7f7a143a9032b5deb1cb0b16e10e4d15
Kyaw Thu <evanch98@gmail.com>
2024-02-13 12:43:35 +0800

    update the BoardCard component

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,5 +1,36 @@
 "use client";
 
-export const BoardCard = () => {
-  return <div>Board Card</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  authorName: string;
+  authorId: string;
+  createdAt: number;
+  imageUrl: string;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({
+  id,
+  title,
+  authorId,
+  authorName,
+  createdAt,
+  imageUrl,
+  orgId,
+  isFavorite,
+}: BoardCardProps) => {
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+        </div>
+      </div>
+    </Link>
+  );
 };
