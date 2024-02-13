c664e205f1ea91999dc04948fee53559cc5b6bdb
Kyaw Thu <evanch98@gmail.com>
2024-02-13 13:54:00 +0800

    add a new component Actions

---
+++ components/actions.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  return <div>Actions</div>;
+};
