2dfd1d6983cf39ae8c7fb1f5a9bc7942ef76c6fc
Kyaw Thu <evanch98@gmail.com>
2024-02-13 13:01:47 +0800

    update the Footer component

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -18,5 +18,24 @@ export const Footer = ({
   onClick,
   disabled,
 }: FooterProps) => {
-  return <div>Footer</div>;
+  return (
+    <div className="relative bg-white p-3">
+      <p className="text-[13px] truncate max-w-[calc(100%-20px)]">{title}</p>
+      <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
+        {authorLabel}, {createdAtLabel}
+      </p>
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className={cn(
+          "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
+          disabled && "cursor-not-allowed opacity-75"
+        )}
+      >
+        <Star
+          className={cn("h-4 w-4", isFavorite && "fill-blue-600 text-blue-600")}
+        />
+      </button>
+    </div>
+  );
 };
