2de5ea43beeb07001fbbef50fcafdd5af00af0de
Kyaw Thu <evanch98@gmail.com>
2024-02-14 11:47:46 +0800

    add remove function

--- convex/board.ts
+++ convex/board.ts
@@ -39,3 +39,18 @@ export const create = mutation({
     return board;
   },
 });
+
+export const remove = mutation({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    // TODO: Later check to delete favorite relation
+
+    await ctx.db.delete(args.id);
+  },
+});
