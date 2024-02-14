c5b63a84e96c3673f7b01abaa55bd984290c27f9
Kyaw Thu <evanch98@gmail.com>
2024-02-14 12:14:19 +0800

    add update function

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
+import { title } from "process";
 
 const images = [
   "/placeholders/1.svg",
@@ -69,5 +70,43 @@ export const remove = mutation({
     // TODO: Later check to delete favorite relation
 
     await ctx.db.delete(args.id);
+  },
+});
+
+/**
+ * Api endpoint for updating the title of a board.
+ * The function requires two arguments
+ *   board id and
+ *   title of type string
+ * The function 'update' will check if the user has logged in.
+ * If the user has not logged in, it will throw an error.
+ * The function 'update' will also check if the title is provided.
+ * If the title is not provided, it will throw an error.
+ * The function 'update' will also check if the characters of the title is more than 60.
+ * If the characters are more than 60, it will throw an error.
+ * Otherwise, it will update the title of the board.
+ */
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const title = args.title.trim();
+
+    if (!title) {
+      throw new Error("Title is required");
+    }
+
+    if (title.length > 60) {
+      throw new Error("Title cannot be longer than 60 characters.");
+    }
+
+    const board = await ctx.db.patch(args.id, { title: args.title });
+
+    return board;
   },
 });
