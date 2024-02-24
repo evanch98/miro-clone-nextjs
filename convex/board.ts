5bc5b048a38f26996b742438516db217272e794f
Kyaw Thu <evanch98@gmail.com>
2024-02-24 19:21:45 +0800

    update the favorite function

--- convex/board.ts
+++ convex/board.ts
@@ -155,8 +155,8 @@ export const favorite = mutation({
     // To check if the user has already favorited the board
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex("by_user_board_org", (q) =>
-        q.eq("userId", userId).eq("boardId", board._id).eq("orgId", arg.orgId)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
