d379a8ef3c3b54b0c8ea9f84361990da1a8a93eb
Kyaw Thu <evanch98@gmail.com>
2024-02-24 15:12:26 +0800

    add a new table userFavorites

--- convex/schema.ts
+++ convex/schema.ts
@@ -14,4 +14,13 @@ export default defineSchema({
       searchField: "title",
       filterFields: ["orgId"],
     }),
+  userFavorites: defineTable({
+    orgId: v.string(),
+    userId: v.string(),
+    boardId: v.string(),
+  })
+    .index("by_board", ["boardId"])
+    .index("by_user_org", ["userId", "orgId"])
+    .index("by_user_board", ["userId", "boardId"])
+    .index("by_user_board_org", ["userId", "boardId", "orgId"]),
 });
