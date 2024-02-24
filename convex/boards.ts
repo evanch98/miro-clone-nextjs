254bd36835e0af1ce2e0273e32d74808969a7aa7
Kyaw Thu <evanch98@gmail.com>
2024-02-24 19:25:42 +0800

    update the get function

--- convex/boards.ts
+++ convex/boards.ts
@@ -4,6 +4,7 @@ import { query } from "./_generated/server";
 export const get = query({
   args: {
     orgId: v.string(),
+    search: v.optional(v.string()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
@@ -12,11 +13,21 @@ export const get = query({
       throw new Error("Unauthorized");
     }
 
-    const boards = await ctx.db
-      .query("boards")
-      .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
-      .order("desc")
-      .collect();
+    const title = args.search as string;
+    let boards = [];
+
+    if (title) {
+      boards = await ctx.db
+        .query("boards")
+        .withSearchIndex("search_title", (q) => q.search("title", title))
+        .collect();
+    } else {
+      boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+    }
 
     const boardsWithFavoriteRelation = boards.map((board) => {
       return ctx.db
