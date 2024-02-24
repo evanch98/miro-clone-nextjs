aacd88dda72fdf772de0b985df80796e6605a087
Kyaw Thu <evanch98@gmail.com>
2024-02-24 15:43:14 +0800

    update the boards.ts

--- convex/boards.ts
+++ convex/boards.ts
@@ -6,7 +6,7 @@ export const get = query({
     orgId: v.string(),
   },
   handler: async (ctx, args) => {
-    const identity = ctx.auth.getUserIdentity();
+    const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
       throw new Error("Unauthorized");
@@ -18,6 +18,20 @@ export const get = query({
       .order("desc")
       .collect();
 
-    return boards;
+    const boardsWithFavoriteRelation = boards.map((board) => {
+      return ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) =>
+          q.eq("userId", identity.subject).eq("boardId", board._id)
+        )
+        .unique()
+        .then((favorite) => {
+          return { ...board, isFavorite: !!favorite };
+        });
+    });
+
+    const boardsWithFavoriteBoolean = Promise.all(boardsWithFavoriteRelation);
+
+    return boardsWithFavoriteBoolean;
   },
 });
