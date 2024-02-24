496f651820e4f84e22fb89ea0a606dec3ab194c0
Kyaw Thu <evanch98@gmail.com>
2024-02-24 19:52:32 +0800

    update the NewBoardButton component

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -4,6 +4,7 @@ import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 interface NewBoardButtonProps {
@@ -12,6 +13,7 @@ interface NewBoardButtonProps {
 }
 
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const router = useRouter();
   const { mutate, pending } = useApiMutation(api.board.create);
 
   const onClick = () => {
@@ -21,7 +23,7 @@ export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
     })
       .then((id) => {
         toast.success("Board created");
-        // TODO: Redirect to /board/{}
+        router.push(`/board/${id}`);
       })
       .catch(() => toast.error("Failed to create board"));
   };
