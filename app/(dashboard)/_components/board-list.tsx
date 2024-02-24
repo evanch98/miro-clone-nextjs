85531348a7b1de3b13e0d18ec029121cccab6be7
Kyaw Thu <evanch98@gmail.com>
2024-02-24 15:44:42 +0800

    update the BoardList component

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -68,7 +68,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
             authorName={board.authorName}
             createdAt={board._creationTime}
             orgId={board.orgId}
-            isFavorite={false}
+            isFavorite={board.isFavorite}
           />
         ))}
       </div>
