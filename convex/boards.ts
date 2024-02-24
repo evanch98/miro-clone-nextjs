c4ec5a5fe7cd035797a557e82b456da8203eee4b
Kyaw Thu <evanch98@gmail.com>
2024-02-24 19:30:32 +0800

    update the boards.ts

--- convex/boards.ts
+++ convex/boards.ts
@@ -5,12 +5,25 @@ export const get = query({
   args: {
     orgId: v.string(),
     search: v.optional(v.string()),
+    favorites: v.optional(v.string()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
       throw new Error("Unauthorized");
+    }
+
+    if (args.favorites) {
+      const favoritedBoard = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_org", (q) =>
+          q.eq("userId", identity.subject).eq("orgId", args.orgId)
+        )
+        .order("desc")
+        .collect();
+
+      const ids = favoritedBoard.map((b) => b._id);
     }
 
     const title = args.search as string;
