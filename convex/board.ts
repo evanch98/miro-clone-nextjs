4b97d53e3d1ab2b3f829f66ccaddcb4cea899124
Kyaw Thu <evanch98@gmail.com>
2024-02-24 19:20:31 +0800

    update the remove function

--- convex/board.ts
+++ convex/board.ts
@@ -66,7 +66,19 @@ export const remove = mutation({
       throw new Error("Unauthorized");
     }
 
-    // TODO: Later check to delete favorite relation
+    // To delete favorite relation
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", args.id)
+      )
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
 
     await ctx.db.delete(args.id);
   },
