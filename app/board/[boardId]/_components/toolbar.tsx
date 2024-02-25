1e7f4cc588b6e579ff48edf1402e99e7f3c6160c
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:21:48 +0800

    add ToolbarSkeleton

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,5 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
@@ -11,6 +13,14 @@ export const Toolbar = () => {
         <div>Undo</div>
         <div>Redo</div>
       </div>
+    </div>
+  );
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-neutral-200 h-[360px] w-[52px] rounded-md">
+      <Skeleton className="h-full w-full" />
     </div>
   );
 };
