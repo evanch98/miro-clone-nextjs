3cfe871ca0a8b2ce5ff371881923e504954babb3
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:14:13 +0800

    add Info.Skeleton

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,3 +1,5 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
@@ -5,3 +7,11 @@ export const Info = () => {
     </div>
   );
 };
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]">
+      <Skeleton className="h-full w-full bg-neutral-400" />
+    </div>
+  );
+}
