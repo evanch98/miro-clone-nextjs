ff7c002905b6030794d9d709e8e77f600123fdd4
Kyaw Thu <evanch98@gmail.com>
2024-02-25 10:41:57 +0800

    add the Toolbar component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -2,12 +2,14 @@
 
 import { Info } from "./info";
 import { Participants } from "./participants";
+import { Toolbar } from "./toolbar";
 
 export const Canvas = () => {
   return (
     <main className="h-full w-full bg-neutral-100 relative touch-none">
       <Info />
       <Participants />
+      <Toolbar />
     </main>
   );
 };
