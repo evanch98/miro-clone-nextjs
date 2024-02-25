d277e320afbca9fea4a31bdb4614308c378dd838
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:19:35 +0800

    update the Loading component

--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -1,11 +1,13 @@
 import { Loader } from "lucide-react";
 import { Info } from "./info";
+import { Participants } from "./participants";
 
 export const Loading = () => {
   return (
     <main className="h-full w-full bg-neutral-100 relative touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
       <Info.Skeleton />
+      <Participants.Skeleton />
     </main>
   );
 };
