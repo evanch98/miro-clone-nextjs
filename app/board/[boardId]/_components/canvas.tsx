bbf9699a4cfed3ae53ad7ddf41ed8cec2d731799
Kyaw Thu <evanch98@gmail.com>
2024-02-25 10:35:32 +0800

    add the Info component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Info } from "./info";
+
 export const Canvas = () => {
   return (
     <main className="h-full w-full bg-neutral-100 relative touch-none">
-      Canvas
+      <Info />
     </main>
   );
 };
