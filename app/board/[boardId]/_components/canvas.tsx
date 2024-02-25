446925de7eae7fa2644bcc705e2270ce2c7d5358
Kyaw Thu <evanch98@gmail.com>
2024-02-25 10:38:58 +0800

    add the Participants component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Info } from "./info";
+import { Participants } from "./participants";
 
 export const Canvas = () => {
   return (
     <main className="h-full w-full bg-neutral-100 relative touch-none">
       <Info />
+      <Participants />
     </main>
   );
 };
