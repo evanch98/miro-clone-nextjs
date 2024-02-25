da08ed683a2a51b1ca37d949e7b3ff0791929498
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:22:47 +0800

    update the Toolbar.Skeleton

--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -1,6 +1,7 @@
 import { Loader } from "lucide-react";
 import { Info } from "./info";
 import { Participants } from "./participants";
+import { Toolbar } from "./toolbar";
 
 export const Loading = () => {
   return (
@@ -8,6 +9,7 @@ export const Loading = () => {
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
       <Info.Skeleton />
       <Participants.Skeleton />
+      <Toolbar.Skeleton />
     </main>
   );
 };
