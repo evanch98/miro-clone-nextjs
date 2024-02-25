4ca7a5b9e66222ca44a81900c24c7cf9fc875c6e
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:18:54 +0800

    add Participants.Skeleton

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
       List of users
+    </div>
+  );
+};
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md flex items-center w-[100px]">
+      <Skeleton className="h-full w-full bg-neutral-200" />
     </div>
   );
 };
