2d6225ddf7f3fb79a0ec73ea45232f5aa2a8ce2d
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:24:57 +0800

    update the BoardIdPage

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -9,8 +9,6 @@ interface BoardIdPageProps {
 }
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return <Loading />;
-
   return (
     <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
