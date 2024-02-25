597605c005510bc2823fb7aa5d98a571f6c38041
Kyaw Thu <evanch98@gmail.com>
2024-02-25 10:45:54 +0800

    update the Toolbar component

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,16 @@
 export const Toolbar = () => {
-  return <div>Toolbar</div>;
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+      <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+        <div>Pencil</div>
+        <div>Square</div>
+        <div>Circle</div>
+        <div>Ellipsis</div>
+      </div>
+      <div className="bg-white rounded-md gap-y-1 p-1.5 flex flex-col items-center shadow-md">
+        <div>Undo</div>
+        <div>Redo</div>
+      </div>
+    </div>
+  );
 };
