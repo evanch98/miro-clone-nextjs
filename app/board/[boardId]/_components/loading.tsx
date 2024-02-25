ee6f3ecdb18bc7d8a89520379458bfadc713301f
Kyaw Thu <evanch98@gmail.com>
2024-02-25 11:15:20 +0800

    add the Info.Skeleton

--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -1,10 +1,11 @@
-import { Skeleton } from "@/components/ui/skeleton";
 import { Loader } from "lucide-react";
+import { Info } from "./info";
 
 export const Loading = () => {
   return (
     <main className="h-full w-full bg-neutral-100 relative touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
+      <Info.Skeleton />
     </main>
   );
 };
