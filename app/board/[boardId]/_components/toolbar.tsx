84e56d7fa6661ba4162a7b83ab717e38d1f78ed5
Kyaw Thu <evanch98@gmail.com>
2024-02-28 12:30:46 +0800

    update the toolbar.ts

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -17,7 +17,7 @@ export const Toolbar = () => {
   );
 };
 
-Toolbar.Skeleton = function ToolbarSkeleton() {
+export const ToolbarSkeleton = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 h-[360px] w-[52px] rounded-md">
       <Skeleton className="h-full w-full bg-neutral-200" />
