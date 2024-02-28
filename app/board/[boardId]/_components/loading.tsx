c6836d3043b385441292ea7317cdfd56ff75f4d9
Kyaw Thu <evanch98@gmail.com>
2024-02-28 12:32:14 +0800

    update the loading.ts

--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -1,15 +1,15 @@
 import { Loader } from "lucide-react";
 import { InfoSkeleton } from "./info";
-import { Participants } from "./participants";
-import { Toolbar } from "./toolbar";
+import { ParticipantsSkeleton } from "./participants";
+import { ToolbarSkeleton } from "./toolbar";
 
 export const Loading = () => {
   return (
     <main className="h-full w-full bg-neutral-100 relative touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
       <InfoSkeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 };
