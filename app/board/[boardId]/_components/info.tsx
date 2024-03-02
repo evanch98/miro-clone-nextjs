f147bd6860e233e9910182f0582b8e504056a9ab
Kyaw Thu <evanch98@gmail.com>
2024-03-02 15:50:25 +0800

    update the Info component

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,15 +1,35 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 interface InfoProps {
   boardId: string;
 }
 
 export const Info = ({ boardId }: InfoProps) => {
+  const data = useQuery(api.board.get, {
+    id: boardId as Id<"boards">,
+  });
+
+  if (!data) return <InfoSkeleton />;
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      TODO: Information about the board
+      <Button className="px-2">
+        <Image src="/logo.svg" alt="Miro Clone Logo" height={32} width={32} />
+      </Button>
     </div>
   );
 };
