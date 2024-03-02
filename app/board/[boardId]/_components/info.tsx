b57a2b3d2b103582bbb9e6f0d5534b5c8ba9df26
Kyaw Thu <evanch98@gmail.com>
2024-03-02 15:42:36 +0800

    update the Info component

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -2,7 +2,11 @@
 
 import { Skeleton } from "@/components/ui/skeleton";
 
-export const Info = () => {
+interface InfoProps {
+  boardId: string;
+}
+
+export const Info = ({ boardId }: InfoProps) => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
       TODO: Information about the board
