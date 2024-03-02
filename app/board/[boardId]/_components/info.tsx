a8322db80a10eb3158d97661dab274c9e376407f
Kyaw Thu <evanch98@gmail.com>
2024-03-02 18:06:26 +0800

    update the Info component

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,24 +1,35 @@
 "use client";
 
+import { Actions } from "@/components/actions";
+import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useRenameModal } from "@/store/use-rename-modal";
 import { useQuery } from "convex/react";
+import { Menu } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const font = Poppins({
   subsets: ["latin"],
   weight: ["600"],
 });
 
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5">|</div>;
+};
+
 interface InfoProps {
   boardId: string;
 }
 
 export const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+
   const data = useQuery(api.board.get, {
     id: boardId as Id<"boards">,
   });
@@ -27,9 +38,46 @@ export const Info = ({ boardId }: InfoProps) => {
 
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      <Button className="px-2">
-        <Image src="/logo.svg" alt="Miro Clone Logo" height={32} width={32} />
-      </Button>
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild className="px-2" variant="board">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Miro Clone Logo"
+              height={32}
+              width={32}
+            />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Miro Clone
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          onClick={() => onOpen(data._id, data.title)}
+          variant="board"
+          className="text-base font-normal px-2"
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant="board">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
