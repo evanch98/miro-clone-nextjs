c23af44d38c0d9ac0cc2376069f0da1acff1ec85
Kyaw Thu <evanch98@gmail.com>
2024-03-02 19:04:08 +0800

    add a new component ToolButton

---
+++ app/board/[boardId]/_components/tool-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { LucideIcon } from "lucide-react";
+
+interface ToolButtonProps {
+  label: string;
+  icon: LucideIcon;
+  onClick: () => void;
+  isActive?: boolean;
+  isDisabled?: boolean;
+}
+
+export const ToolButton = ({
+  label,
+  icon: Icon,
+  onClick,
+  isActive,
+  isDisabled,
+}: ToolButtonProps) => {
+  return (
+    <Hint label={label} side="right" sideOffset={14}>
+      <Button
+        disabled={isDisabled}
+        onClick={onClick}
+        size="icon"
+        variant={isActive ? "boardActive" : "board"}
+      >
+        <Icon />
+      </Button>
+    </Hint>
+  );
+};
