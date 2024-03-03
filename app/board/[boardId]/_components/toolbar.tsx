c6f84d7bad1ef3a8021ef346c5f6b0736c135440
Kyaw Thu <evanch98@gmail.com>
2024-03-03 12:56:28 +0800

    update the Toolbar component

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -35,14 +35,14 @@ export const Toolbar = ({
         <ToolButton
           label="Select"
           icon={MousePointer2}
-          onClick={() => setCanvasState({mode: CanvasMode.None})}
+          onClick={() => setCanvasState({ mode: CanvasMode.None })}
           isActive={canvasState.mode === CanvasMode.None}
         />
         <ToolButton
           label="Text"
           icon={Type}
-          onClick={() => {}}
-          isActive={false}
+          onClick={() => setCanvasState({ mode: CanvasMode.Inserting })}
+          isActive={canvasState.mode === CanvasMode.Inserting}
         />
         <ToolButton
           label="Sticky note"
