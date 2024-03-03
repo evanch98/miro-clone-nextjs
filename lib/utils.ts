0f99281f10ba3eea4e32506fa7d06311040a9654
Kyaw Thu <evanch98@gmail.com>
2024-03-03 13:43:28 +0800

    add a new utils function pointerEventToCanvasPoint

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { Camera } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -9,4 +10,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
+}
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
 }
