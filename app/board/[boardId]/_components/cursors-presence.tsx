87600499ea7c18552d0ba00a23f95d7ffd05c9cb
Kyaw Thu <evanch98@gmail.com>
2024-03-03 13:24:22 +0800

    add CursorsPresence component

---
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { memo } from "react";
+
+export const CursorsPresence = memo(() => {
+  return (
+    <>
+      <p>Cursors</p>
+    </>
+  );
+});
+
+CursorsPresence.displayName = "CursorsPresence";
