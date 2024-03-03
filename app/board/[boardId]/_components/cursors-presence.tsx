44dbd3e6f22b07d9d3c37190d0338a8fa3fd309d
Kyaw Thu <evanch98@gmail.com>
2024-03-03 13:26:50 +0800

    update the cursors-presence.tsx

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { useOthersConnectionIds } from "@/liveblocks.config";
 import { memo } from "react";
+
+const Cursors = () => {
+  const ids = useOthersConnectionIds();
+
+  return (
+    <>
+      {ids.map((connectionId) => {
+        <Cursor key={connectionId} connectionId={connectionId} />;
+      })}
+    </>
+  );
+};
 
 export const CursorsPresence = memo(() => {
   return (
     <>
-      <p>Cursors</p>
+      <Cursors />
     </>
   );
 });
