fc7af4640d52090bb4d917a0408ed05cdb678660
Kyaw Thu <evanch98@gmail.com>
2024-03-03 13:49:11 +0800

    update the liveblocks.config.ts

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,6 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
+  throttle: 16,
   authEndpoint: "/api/liveblocks-auth",
 });
 
@@ -9,7 +10,7 @@ const client = createClient({
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  cursor: { x: number, y: number } | null,
+  cursor: { x: number; y: number } | null;
   // ...
 };
 
