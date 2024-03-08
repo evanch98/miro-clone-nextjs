7eddef0dbe435d31e5fee529341ebb880dbcd53e
Kyaw Thu <evanch98@gmail.com>
2024-03-09 00:09:31 +0800

    update the LayerPreview component

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import { useStorage } from "@/liveblocks.config";
+import { LayerType } from "@/types/canvas";
+import { memo } from "react";
+
 interface LayerPreviewProps {
   id: string;
   onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
   selectionColor?: string;
 }
 
-export const LayerPreview = ({
-  id,
-  onLayerPointerDown,
-  selectionColor,
-}: LayerPreviewProps) => {
-  return <div></div>;
-};
+export const LayerPreview = memo(
+  ({ id, onLayerPointerDown, selectionColor }: LayerPreviewProps) => {
+    const layer = useStorage((root) => root.layers.get(id));
+
+    if (!layer) {
+      return null;
+    }
+
+    switch (layer.type) {
+      case LayerType.Rectangle:
+        return <div>Rectangle</div>;
+      default:
+        console.warn("Unknown layer type");
+        return null
+    }
+  }
+);
+
+LayerPreview.displayName = "LayerPreview";
