28702b7ea6f0f9a1b765e96ae241d45360b55175
Kyaw Thu <evanch98@gmail.com>
2024-03-09 21:28:50 +0800

    update the SelectionBox component

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import { useSelf, useStorage } from "@/liveblocks.config";
+import { LayerType, Side, XYWH } from "@/types/canvas";
 import { memo } from "react";
 
 interface SelectionBoxProps {
-  onResizeHandlePointerDown: () => void;
+  onResizeHandlePointerDown: (corner: Side, initialBounds: XYWH) => void;
 }
+
+const HANDLE_WIDTH = 8;
 
 export const SelectionBox = memo(
   ({ onResizeHandlePointerDown }: SelectionBoxProps) => {
+    const soleLayerId = useSelf((me) =>
+      me.presence.selection.length === 1 ? me.presence.selection[0] : null
+    );
+
+    const isShowingHandles = useStorage(
+      (root) =>
+        soleLayerId && root.layers.get(soleLayerId)?.type !== LayerType.Path
+    );
+
     return <div></div>;
   }
 );
