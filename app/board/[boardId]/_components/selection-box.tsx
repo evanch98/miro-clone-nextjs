0950cbb288ee594e766dc9eefb24c728d20d34dc
Kyaw Thu <evanch98@gmail.com>
2024-03-09 22:59:17 +0800

    update the SelectionBox component

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -127,6 +127,57 @@ export const SelectionBox = memo(
                 // TODO; Add resize handler
               }}
             />
+            <rect
+              className="fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "ns-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${
+                  bounds.x + bounds.width / 2 - HANDLE_WIDTH / 2
+                }px, ${bounds.y - HANDLE_WIDTH / 2 + bounds.height}px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation;
+                // TODO; Add resize handler
+              }}
+            />
+            <rect
+              className="fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "nesw-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${bounds.x - HANDLE_WIDTH / 2}px, ${
+                  bounds.y - HANDLE_WIDTH / 2 + bounds.height
+                }px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation;
+                // TODO; Add resize handler
+              }}
+            />
+            <rect
+              className="fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "ew-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${bounds.x - HANDLE_WIDTH / 2}px, ${
+                  bounds.y - HANDLE_WIDTH / 2 + bounds.height / 2
+                }px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation;
+                // TODO; Add resize handler
+              }}
+            />
           </>
         )}
       </>
