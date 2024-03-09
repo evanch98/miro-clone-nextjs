81fd48ff588e7b21f41085aa256e4f97c9d1db92
Kyaw Thu <evanch98@gmail.com>
2024-03-09 21:50:10 +0800

    update the SelectionBox component

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useSelf, useStorage } from "@/liveblocks.config";
 import { LayerType, Side, XYWH } from "@/types/canvas";
 import { memo } from "react";
@@ -21,7 +22,26 @@ export const SelectionBox = memo(
         soleLayerId && root.layers.get(soleLayerId)?.type !== LayerType.Path
     );
 
-    return <div></div>;
+    const bounds = useSelectionBounds();
+
+    if (!bounds) {
+      return null;
+    }
+
+    return (
+      <>
+        <rect
+          className="fill-transparent stroke-blue-500 stroke-1 pointer-events-none"
+          style={{
+            transform: `translate(${bounds.x}px, ${bounds.y}px)`,
+          }}
+          x={0}
+          y={0}
+          width={bounds.width}
+          height={bounds.height}
+        />
+      </>
+    );
   }
 );
 
