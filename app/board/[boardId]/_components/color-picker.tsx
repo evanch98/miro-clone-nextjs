de21e0b54d49fc5d6915b4543a1604d674da06b0
Kyaw Thu <evanch98@gmail.com>
2024-03-10 16:21:14 +0800

    update the ColorPicker component

--- app/board/[boardId]/_components/color-picker.tsx
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { colorToCss } from "@/lib/utils";
 import { Color } from "@/types/canvas";
 
 interface ColorPickerProps {
@@ -7,5 +8,35 @@ interface ColorPickerProps {
 }
 
 export const ColorPicker = ({ onChange }: ColorPickerProps) => {
-  return <div>Color Pickers</div>;
+  return (
+    <div className="flex flex-wrap gap-2 items-center max-w-[164px] pr-2 mr-2 border-r border-neutral-200">
+      <ColorButton color={{ r: 243, g: 82, b: 35 }} onClick={onChange} />
+      <ColorButton color={{ r: 255, g: 249, b: 177 }} onClick={onChange} />
+      <ColorButton color={{ r: 68, g: 202, b: 99 }} onClick={onChange} />
+      <ColorButton color={{ r: 39, g: 142, b: 237 }} onClick={onChange} />
+      <ColorButton color={{ r: 155, g: 105, b: 245 }} onClick={onChange} />
+      <ColorButton color={{ r: 252, g: 142, b: 42 }} onClick={onChange} />
+      <ColorButton color={{ r: 0, g: 0, b: 0 }} onClick={onChange} />
+      <ColorButton color={{ r: 255, g: 255, b: 255 }} onClick={onChange} />
+    </div>
+  );
+};
+
+interface ColorButtonProps {
+  onClick: (color: Color) => void;
+  color: Color;
+}
+
+const ColorButton = ({ onClick, color }: ColorButtonProps) => {
+  return (
+    <button
+      className="w-8 h-8 items-center flex justify-center hover:opacity-75 transition"
+      onClick={() => onClick(color)}
+    >
+      <div
+        className="h-8 w-8 rounded-md border border-neutral-300"
+        style={{ background: colorToCss(color) }}
+      />
+    </button>
+  );
 };
--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -4,6 +4,7 @@ import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useSelf } from "@/liveblocks.config";
 import { Camera, Color } from "@/types/canvas";
 import { memo } from "react";
+import { ColorPicker } from "./color-picker";
 
 interface SelectionToolsProps {
   camera: Camera;
