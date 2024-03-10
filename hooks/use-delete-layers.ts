f829b6c2ba4474165438778033c52e57b1e5eb09
Kyaw Thu <evanch98@gmail.com>
2024-03-10 16:28:37 +0800

    add a new hook use-delete-layers

---
+++ hooks/use-delete-layers.ts
@@ -0,0 +1,21 @@
+import { useMutation, useSelf } from "@/liveblocks.config";
+
+export const useDeleteLayers = () => {
+  const selection = useSelf((me) => me.presence.selection);
+
+  return useMutation(({ storage, setMyPresence }) => {
+    const liveLayers = storage.get("layers");
+    const liveLayersIds = storage.get("layerIds");
+
+    for (const id of selection) {
+      liveLayers.delete(id);
+      const index = liveLayersIds.indexOf(id);
+
+      if (index !== -1) {
+        liveLayersIds.delete(index);
+      }
+    }
+
+    setMyPresence({ selection: [] }, { addToHistory: true });
+  }, []);
+};
