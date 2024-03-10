264cb52937ea3d5f1dae3673c810257c77e178e6
Kyaw Thu <evanch98@gmail.com>
2024-03-10 16:31:53 +0800

    update the SelectionTools component

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -5,6 +5,10 @@ import { useMutation, useSelf } from "@/liveblocks.config";
 import { Camera, Color } from "@/types/canvas";
 import { memo } from "react";
 import { ColorPicker } from "./color-picker";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -27,6 +31,8 @@ export const SelectionTools = memo(
       [selection, setLastUsedColor]
     );
 
+    const deleteLayers = useDeleteLayers();
+
     const selectionBounds = useSelectionBounds();
 
     if (!selectionBounds) {
@@ -44,6 +50,13 @@ export const SelectionTools = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
+          <Hint label="Delete">
+            <Button variant="board" size="icon" onClick={deleteLayers}>
+              <Trash2 />
+            </Button>
+          </Hint>
+        </div>
       </div>
     );
   }
