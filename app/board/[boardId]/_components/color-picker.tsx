eacb5efa428bba3e7ceb1f375bad4da556e4c9fc
Kyaw Thu <evanch98@gmail.com>
2024-03-10 16:13:03 +0800

    add a new component ColorPicker

---
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Color } from "@/types/canvas";
+
+interface ColorPickerProps {
+  onChange: (color: Color) => void;
+}
+
+export const ColorPicker = ({ onChange }: ColorPickerProps) => {
+  return <div>Color Pickers</div>;
+};
