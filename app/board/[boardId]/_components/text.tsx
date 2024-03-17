f34fe437e276c12aa0f6fddf6ec312e65e33c742
Kyaw Thu <evanch98@gmail.com>
2024-03-17 12:50:46 +0800

    add a new component Text

---
+++ app/board/[boardId]/_components/text.tsx
@@ -0,0 +1,38 @@
+import { cn, colorToCss } from "@/lib/utils";
+import { TextLayer } from "@/types/canvas";
+import { useMutation } from "@/liveblocks.config";
+import { Kalam } from "next/font/google";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+
+const font = Kalam({ subsets: ["latin"], weight: "400" });
+
+interface TextProps {
+  id: string;
+  layer: TextLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Text = ({
+  layer,
+  onPointerDown,
+  id,
+  selectionColor,
+}: TextProps) => {
+  const { x, y, width, height, fill, value } = layer;
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      height={height}
+      width={width}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+      }}
+    >
+      <ContentEditable html="Text" onChange={() => {}} />
+    </foreignObject>
+  );
+};
