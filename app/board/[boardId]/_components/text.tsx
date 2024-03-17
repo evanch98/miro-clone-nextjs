c5dbbdf6f03d77f01a8460add2f8a56ba0ec2a13
Kyaw Thu <evanch98@gmail.com>
2024-03-17 13:03:58 +0800

    update the Text component

--- app/board/[boardId]/_components/text.tsx
+++ app/board/[boardId]/_components/text.tsx
@@ -6,6 +6,15 @@ import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 
 const font = Kalam({ subsets: ["latin"], weight: "400" });
 
+const calculateFontSize = (width: number, height: number) => {
+  const maxFontSize = 96;
+  const scaleFactor = 0.5;
+  const fontSizeBasedOnHeight = height * scaleFactor;
+  const fontSizeBasedOnWidth = width * scaleFactor;
+
+  return Math.min(fontSizeBasedOnHeight, fontSizeBasedOnWidth, maxFontSize);
+};
+
 interface TextProps {
   id: string;
   layer: TextLayer;
@@ -21,6 +30,16 @@ export const Text = ({
 }: TextProps) => {
   const { x, y, width, height, fill, value } = layer;
 
+  const updateValue = useMutation(({ storage }, newValue: string) => {
+    const liveLayers = storage.get("layers");
+
+    liveLayers.get(id)?.set("value", newValue);
+  }, []);
+
+  const handleContentChange = (e: ContentEditableEvent) => {
+    updateValue(e.target.value);
+  };
+
   return (
     <foreignObject
       x={x}
@@ -32,7 +51,18 @@ export const Text = ({
         outline: selectionColor ? `1px solid ${selectionColor}` : "none",
       }}
     >
-      <ContentEditable html="Text" onChange={() => {}} />
+      <ContentEditable
+        html={value || "Text"}
+        onChange={handleContentChange}
+        className={cn(
+          "h-full w-full flex items-center justify-center drop-shadow-md outline-none",
+          font.className
+        )}
+        style={{
+          color: fill ? colorToCss(fill) : "#000",
+          fontSize: calculateFontSize(width, height),
+        }}
+      />
     </foreignObject>
   );
 };
