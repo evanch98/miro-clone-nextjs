a0a57bce5e4ac476679c7e33100216d86f9ff08e
Kyaw Thu <evanch98@gmail.com>
2024-03-17 12:41:33 +0800

    add a new component Ellipse

---
+++ app/board/[boardId]/_components/ellipse.tsx
@@ -0,0 +1,31 @@
+import { colorToCss } from "@/lib/utils";
+import { EllipseLayer } from "@/types/canvas";
+
+interface EllipseProps {
+  id: string;
+  layer: EllipseLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Ellipse = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: EllipseProps) => {
+  return (
+    <ellipse
+      className="drop-shadow-md"
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{ transform: `translate(${layer.x}px, ${layer.y}px)` }}
+      cx={layer.width / 2}
+      cy={layer.height / 2}
+      rx={layer.width / 2}
+      ry={layer.height / 2}
+      fill={layer.fill ? colorToCss(layer.fill) : "#000"}
+      stroke={selectionColor || "transparent"}
+      strokeWidth="1"
+    />
+  );
+};
