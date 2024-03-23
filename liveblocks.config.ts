c33ea22954d218f8bf859496b4d2708d0dd8658e
Kyaw Thu <evanch98@gmail.com>
2024-03-23 15:00:17 +0800

    update the liveblocks.config.ts

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,4 +1,4 @@
-import { Layer } from "@/types/canvas";
+import { Color, Layer } from "@/types/canvas";
 import {
   createClient,
   LiveList,
@@ -18,6 +18,8 @@ const client = createClient({
 type Presence = {
   cursor: { x: number; y: number } | null;
   selection: string[];
+  pencilDraft: [x: number, y: number, pressure: number][] | null;
+  penColor: Color | null;
   // ...
 };
 
