98de53a2e117d117461974d0df0d8c2580ba3263
Kyaw Thu <evanch98@gmail.com>
2024-03-23 15:33:21 +0800

    update the Path component

--- app/board/[boardId]/_components/path.tsx
+++ app/board/[boardId]/_components/path.tsx
@@ -1,3 +1,4 @@
+import { getSvgPathFromStroke } from "@/lib/utils";
 import getStroke from "perfect-freehand";
 
 interface PathProps {
@@ -17,5 +18,24 @@ export const Path = ({
   onPointerDown,
   stroke,
 }: PathProps) => {
-  return <path />;
+  return (
+    <path
+      className="drop-shadow-md"
+      onPointerDown={onPointerDown}
+      d={getSvgPathFromStroke(
+        getStroke(points, {
+          size: 16,
+          thinning: 0.5,
+          smoothing: 0.5,
+          streamline: 0.5,
+        })
+      )}
+      style={{ transform: `translate(${x}px, ${y}px)` }}
+      x={0}
+      y={0}
+      fill={fill}
+      stroke={stroke}
+      strokeWidth={1}
+    />
+  );
 };
