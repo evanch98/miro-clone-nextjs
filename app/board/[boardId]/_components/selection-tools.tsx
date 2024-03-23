0a32c15c79ab5510cface4f5f2d1f76b8b1b82b3
Kyaw Thu <evanch98@gmail.com>
2024-03-24 00:05:23 +0800

    update the SelectionTools component

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -24,7 +24,7 @@ export const SelectionTools = memo(
         const liveLayerIds = storage.get("layerIds");
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
           if (selection.includes(arr[i])) {
