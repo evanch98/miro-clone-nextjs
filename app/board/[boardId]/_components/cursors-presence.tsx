4975084f3dee1f9e404481977afecbe8c830ab66
Kyaw Thu <evanch98@gmail.com>
2024-03-23 23:51:19 +0800

    update the CursorsPresence

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -5,6 +5,9 @@ import { memo } from "react";
 import { useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
 
 import { Cursor } from "./cursor";
+import { shallow } from "@liveblocks/client";
+import { Path } from "./path";
+import { colorToCss } from "@/lib/utils";
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
@@ -18,11 +21,40 @@ const Cursors = () => {
   );
 };
 
+const Drafts = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
 
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : "#000"}
+            />
+          );
+        }
+
+        return null;
+      })}
+    </>
+  );
+};
 
 export const CursorsPresence = memo(() => {
   return (
     <>
+      <Drafts />
       <Cursors />
     </>
   );
